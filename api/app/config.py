ffb0b0396580a18971777c687e453750fabaede5
FlowerKinPJBoi <pjboi29@gmail.com>
2026-07-16 08:22:59 -0400

    Optimize api/app/config.py for faster map loading

--- api/app/config.py
+++ api/app/config.py
@@ -17,7 +17,7 @@ class Settings(BaseSettings):
     )
 
     app_name: str = "Wonder Codex API"
-    app_version: str = "1.11.1"
+    app_version: str = "1.11.2"
     environment: str = "production"
     database_url: str = ""
     allowed_origins: List[str] = Field(
